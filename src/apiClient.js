1d3253126ec1feba27104875964ab2370776e97b
CuJo33 <77585826+CuJo33@users.noreply.github.com>
2021-09-14 16:00:12 +0100

    first commit

---
+++ src/apiClient.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+const url = "http://localhost:3001/";
+
+export class ApiClient {
+  constructor(tokenProvider, logoutHandler) {
+    this.tokenProvider = tokenProvider;
+    this.logoutHandler = logoutHandler;
+  }
+
+  apiCall(method, url, data) {
+    return axios({
+      method,
+      url,
+      headers: {
+        authorization: this.tokenProvider(),
+      },
+      data,
+    }).catch((error) => {
+      if (error.response.status === 403) {
+        this.logoutHandler();
+        return Promise.reject();
+      } else {
+        throw error;
+      }
+    });
+  }
+
+  getAds() {
+    return this.apiCall("get", url);
+  }
+
+  addAd(name, price) {
+    return this.apiCall("post", url, { name, price });
+  }
+
+  removeAd(id) {
+    return this.apiCall("delete", `${url}${id}`);
+  }
+
+  updateAd(id, name, price) {
+    return this.apiCall("put", `${url}${id}`, { name, price });
+  }
+
+  //   async login(username, password) {
+  //     return await axios({
+  //       method: "post",
+  //       url: `${url}auth`,
+  //       data: {
+  //         username,
+  //         password,
+  //       },
+  //     });
+  //   }
+}
