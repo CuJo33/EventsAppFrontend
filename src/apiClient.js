9f6635d1181827f48c6cbe452a1160d3a001ce8f
CuJo33 <77585826+CuJo33@users.noreply.github.com>
2021-09-20 12:52:45 +0100

    heroku deploy

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:3001/";
+const url = "https://eventsapp123.herokuapp.com/";
 
 export class ApiClient {
   constructor(tokenProvider, logoutHandler) {
